8c699c34be7af134895eacd809a45fa69c5617e7
Rashedul Islam <rashedulraha.bd@gmail.com>
2025-11-15 20:51:15 +0600

    create register api

---
+++ index.js
@@ -0,0 +1,67 @@
+import express from "express";
+import dotenv from "dotenv";
+import bcrypt from "bcrypt";
+import mongodb, { MongoClient } from "mongodb";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const port = process.env.PROT || 3000;
+const client = new MongoClient(process.env.DATA_BASE_URL);
+
+async function run() {
+  // database connection
+
+  try {
+    await client.connect();
+    // console.log("database connection");
+    const db = client.db("crud-operation");
+    const userCollection = db.collection("users");
+
+    //!   user end point
+    app.post("/register", async (req, res) => {
+      try {
+        const { name, email, password } = req.body;
+
+        const existed = await userCollection.findOne({
+          email: email,
+        });
+
+        if (existed) {
+          return res.status(409).json({
+            message: "user already exist",
+          });
+        }
+
+        const passwordHashed = await bcrypt.hash(password, 10);
+
+        const result = await userCollection.insertOne({
+          name,
+          email,
+          password: passwordHashed,
+        });
+        res.status(201).json({
+          message: "user create successfully",
+          result,
+        });
+      } catch (error) {
+        res.status(400).json({
+          message: "field to create user",
+        });
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
